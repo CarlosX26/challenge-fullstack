3ec9cd8292bd38badfb476243b1156f5f811b0b1
Carlos Junio <cjunior1@live.com>
2023-09-05 11:09:41 -0300

    feat: controller update product

--- back-end/src/controllers/product.controllers.ts
+++ back-end/src/controllers/product.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import createProductService from "../services/products/createProduct.service"
 import readProductsService from "../services/products/readProducts.service"
+import updateProductService from "../services/products/updateProduct.service"
 
 const createProductController = async (req: Request, res: Response) => {
   const data = await createProductService(req.body, req.userAuthId)
@@ -14,4 +15,14 @@ const readProductsController = async (req: Request, res: Response) => {
   return res.status(200).json(data)
 }
 
-export { createProductController, readProductsController }
+const updateProductController = async (req: Request, res: Response) => {
+  const data = await updateProductService(req.body, req.params.id)
+
+  return res.status(200).json(data)
+}
+
+export {
+  createProductController,
+  readProductsController,
+  updateProductController,
+}
