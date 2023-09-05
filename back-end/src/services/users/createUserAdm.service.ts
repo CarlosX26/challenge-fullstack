da2653afa5d3d49e8cc35727da799340251939cf
Carlos Junio <cjunior1@live.com>
2023-09-05 10:06:08 -0300

    feat: service create adm user

---
+++ back-end/src/services/users/createUserAdm.service.ts
@@ -0,0 +1,18 @@
+import AppDataSource from "../../data-source"
+import { User } from "../../entities/user.entity"
+import { IUserAdm, IUserReturn } from "../../interfaces/user"
+import { UserReturn } from "../../schemas/user"
+
+const createUserAdmService = async (
+  payload: IUserAdm
+): Promise<IUserReturn> => {
+  const userRepo = AppDataSource.getRepository(User)
+
+  const user = userRepo.create(payload)
+
+  await userRepo.save(user)
+
+  return UserReturn.parse(user)
+}
+
+export default createUserAdmService
