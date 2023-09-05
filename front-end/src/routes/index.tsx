0c6f3a12115ee18b544d58c86fdaa6a8f6e36ca0
Carlos Junio <cjunior1@live.com>
2023-09-05 15:54:27 -0300

    feat: add route adm dashboard

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import { AuthPage } from "../pages/Auth"
+import { Dashboard } from "../pages/Dashboard"
 
 const RoutesIndex = () => {
   return (
     <Routes>
       <Route path="/auth" element={<AuthPage />} />
+      <Route path="/adm/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
