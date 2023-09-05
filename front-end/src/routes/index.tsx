66d04160ff394c0b5db30ce8b848c2dd18879da4
Carlos Junio <cjunior1@live.com>
2023-09-05 16:27:53 -0300

    feat: add home page in route

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import { AuthPage } from "../pages/Auth"
 import { Dashboard } from "../pages/Dashboard"
+import { Home } from "../pages/Home"
 
 const RoutesIndex = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/auth" element={<AuthPage />} />
       <Route path="/adm/dashboard" element={<Dashboard />} />
     </Routes>
