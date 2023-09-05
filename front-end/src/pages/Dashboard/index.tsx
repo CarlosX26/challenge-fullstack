02241bf5ea419f7808313cff1a411fe68a1d9291
Carlos Junio <cjunior1@live.com>
2023-09-05 20:52:43 -0300

    feat: refactor logic for compone

--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -1,62 +1,18 @@
-import { ComponentWithAs, Flex, Heading, IconProps } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
 import { Header } from "../../components/Header"
 import { Container } from "../../components/Container"
-import { AddIcon, EditIcon, DeleteIcon } from "@chakra-ui/icons"
+import { ModalAddProduct } from "../../components/Modal/ModalAddProduct"
+import { DashboardOptions } from "../../components/DashboardOptions"
 
 export const Dashboard = () => {
-  const options: {
-    name: string
-    icon: ComponentWithAs<"svg", IconProps>
-  }[] = [
-    {
-      name: "Adicionar produto",
-      icon: AddIcon,
-    },
-    {
-      name: "Editar produto",
-      icon: EditIcon,
-    },
-    {
-      name: "Deletar produto",
-      icon: DeleteIcon,
-    },
-  ]
-
   return (
     <>
       <Header />
       <Container>
         <Heading py="32px">Controle</Heading>
-        <Flex as={"ul"} gap="1rem" flexDir={{ base: "column", md: "row" }}>
-          {options.map((option) => (
-            <Flex
-              role="group"
-              key={option.name}
-              as={"li"}
-              flexDir="column"
-              justifyContent="center"
-              alignItems="center"
-              border="2px"
-              borderColor="teal.600"
-              borderRadius="md"
-              w={{ base: "100%", md: "176px" }}
-              h="176px"
-              p="16px"
-              gap="16px"
-              cursor="pointer"
-              _hover={{
-                bg: "teal.600",
-              }}
-            >
-              <Heading fontSize="md" _groupHover={{ color: "white" }}>
-                {option.name}
-              </Heading>
-
-              {<option.icon w="8" h="8" _groupHover={{ color: "white" }} />}
-            </Flex>
-          ))}
-        </Flex>
+        <DashboardOptions />
       </Container>
+      <ModalAddProduct />
     </>
   )
 }
