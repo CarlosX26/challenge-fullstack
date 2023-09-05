368d52ad95a2fe9ad8e5424052992d2e1d0ffa75
Carlos Junio <cjunior1@live.com>
2023-09-05 11:10:06 -0300

    feat: interface update product

--- back-end/src/interfaces/product.ts
+++ back-end/src/interfaces/product.ts
@@ -1,8 +1,10 @@
 import { z } from "zod"
-import { Product, ProductReturn } from "../schemas/product"
+import { Product, ProductReturn, ProductUpdate } from "../schemas/product"
 
 type IProduct = z.infer<typeof Product>
 
+type IProductUpdate = z.infer<typeof ProductUpdate>
+
 type IProductReturn = z.infer<typeof ProductReturn>
 
-export type { IProduct, IProductReturn }
+export type { IProduct, IProductReturn, IProductUpdate }
