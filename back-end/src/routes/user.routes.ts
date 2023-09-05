c55d7a29c5ef61af0bdf02371bd866b7559b3d19
Carlos Junio <cjunior1@live.com>
2023-09-05 10:07:04 -0300

    feat: route create adm user

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express"
 import { verifySchemaMiddleware } from "../middlewares/verifySchema.middleware"
 import { User } from "../schemas/user"
-import { createUserController } from "../controllers/user.controllers"
+import {
+  createUserAdmController,
+  createUserController,
+} from "../controllers/user.controllers"
 
 const userRouter = Router()
 
 userRouter.post("", verifySchemaMiddleware(User), createUserController)
+userRouter.post("/admin", verifySchemaMiddleware(User), createUserAdmController)
 
 export default userRouter
