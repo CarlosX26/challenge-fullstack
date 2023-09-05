a30ac638081961d4a39c1d651389acc68ce9276d
Carlos Junio <cjunior1@live.com>
2023-09-05 10:17:11 -0300

    feat: interface auth user

--- back-end/src/interfaces/user.ts
+++ back-end/src/interfaces/user.ts
@@ -1,10 +1,12 @@
 import { z } from "zod"
-import { User, UserAdm, UserReturn } from "../schemas/user"
+import { User, UserAdm, UserAuth, UserReturn } from "../schemas/user"
 
 type IUser = z.infer<typeof User>
+
+type IUserAuth = z.infer<typeof UserAuth>
 
 type IUserAdm = z.infer<typeof UserAdm>
 
 type IUserReturn = z.infer<typeof UserReturn>
 
-export type { IUser, IUserReturn, IUserAdm }
+export type { IUser, IUserReturn, IUserAdm, IUserAuth }
