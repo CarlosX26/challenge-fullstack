98fbe63811ef11d11353a4ab79159fd0a7779c23
Carlos Junio <cjunior1@live.com>
2023-09-05 11:03:10 -0300

    feat: service read all products

--- back-end/src/controllers/product.controllers.ts
+++ back-end/src/controllers/product.controllers.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express"
 import createProductService from "../services/products/createProduct.service"
+import readProductsService from "../services/products/readProducts.service"
 
 const createProductController = async (req: Request, res: Response) => {
   const data = await createProductService(req.body, req.userAuthId)
 
+  return res.status(201).json(data)
+}
+
+const readProductsController = async (req: Request, res: Response) => {
+  const data = await readProductsService()
+
   return res.status(200).json(data)
 }
 
-export { createProductController }
+export { createProductController, readProductsController }
