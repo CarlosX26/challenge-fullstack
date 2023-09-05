05ef994e7950c6bf3fe33047be55ae4845fd7325
Carlos Junio <cjunior1@live.com>
2023-09-05 10:06:24 -0300

    feat: controller create adm user

--- back-end/src/controllers/user.controllers.ts
+++ back-end/src/controllers/user.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import createUserService from "../services/users/createUser.service"
+import createUserAdmService from "../services/users/createUserAdm.service"
 
 const createUserController = async (req: Request, res: Response) => {
   const data = await createUserService(req.body)
@@ -7,4 +8,10 @@ const createUserController = async (req: Request, res: Response) => {
   return res.status(201).json(data)
 }
 
-export { createUserController }
+const createUserAdmController = async (req: Request, res: Response) => {
+  const data = await createUserAdmService(req.body)
+
+  return res.status(201).json(data)
+}
+
+export { createUserController, createUserAdmController }
