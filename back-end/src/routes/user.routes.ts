3bd79acf8ff9b5a6a0467d12fe5077445507bd5e
Carlos Junio <cjunior1@live.com>
2023-09-05 10:17:25 -0300

    feat: route auth user

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express"
 import { verifySchemaMiddleware } from "../middlewares/verifySchema.middleware"
-import { User } from "../schemas/user"
+import { User, UserAuth } from "../schemas/user"
 import {
+  authUserController,
   createUserAdmController,
   createUserController,
 } from "../controllers/user.controllers"
 
 const userRouter = Router()
 
+userRouter.post("/auth", verifySchemaMiddleware(UserAuth), authUserController)
 userRouter.post("", verifySchemaMiddleware(User), createUserController)
 userRouter.post("/admin", verifySchemaMiddleware(User), createUserAdmController)
 
