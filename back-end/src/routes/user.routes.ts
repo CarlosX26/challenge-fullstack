60e2cc4f42c1ff80f68b478d19698495353022ea
Carlos Junio <cjunior1@live.com>
2023-09-05 10:25:16 -0300

    feat: route update user

--- back-end/src/routes/user.routes.ts
+++ back-end/src/routes/user.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from "express"
 import { verifySchemaMiddleware } from "../middlewares/verifySchema.middleware"
-import { User, UserAuth } from "../schemas/user"
+import { User, UserAuth, UserUpdate } from "../schemas/user"
 import {
   authUserController,
   createUserAdmController,
   createUserController,
+  updateUserController,
 } from "../controllers/user.controllers"
+import { verifyUserAuthMiddleware } from "../middlewares/verifyUserAuth.middleware"
 
 const userRouter = Router()
 
 userRouter.post("/auth", verifySchemaMiddleware(UserAuth), authUserController)
 userRouter.post("", verifySchemaMiddleware(User), createUserController)
 userRouter.post("/admin", verifySchemaMiddleware(User), createUserAdmController)
+userRouter.patch(
+  "/profile",
+  verifyUserAuthMiddleware,
+  verifySchemaMiddleware(UserUpdate),
+  updateUserController
+)
 
 export default userRouter
