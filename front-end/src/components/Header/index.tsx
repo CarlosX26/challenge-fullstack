ca848c9eb2b19f9948fe72ec0b88a3d9d0a1cde9
Carlos Junio <cjunior1@live.com>
2023-09-05 20:12:36 -0300

    feat: logic header for change content

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -1,7 +1,25 @@
-import { Avatar, Flex } from "@chakra-ui/react"
+import {
+  Avatar,
+  Flex,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react"
 import { Logo } from "../Logo"
+import { Link, useLocation } from "react-router-dom"
+import { useAuthContext } from "../../contexts/authContext"
+import { SearchIcon } from "@chakra-ui/icons"
 
 export const Header = () => {
+  const location = useLocation()
+
+  const { user, logout } = useAuthContext()
+
+  const isAdm = location.pathname.includes("adm")
+
   return (
     <Flex as={"header"} w="100%" borderBottom="2px" borderColor="teal.600">
       <Flex
@@ -14,7 +32,30 @@ export const Header = () => {
       >
         <Logo />
 
-        <Avatar name={"CJ"} />
+        <Flex alignItems="center" gap="16px">
+          {!isAdm && (
+            <Flex gap="16px">
+              <Input
+                type="text"
+                placeholder="Pesquise um item"
+                variant="filled"
+              />
+              <IconButton aria-label="Search product" icon={<SearchIcon />} />
+            </Flex>
+          )}
+
+          {user && (
+            <Menu>
+              <MenuButton>
+                <Avatar name={user?.name} />
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={logout}>Sair</MenuItem>
+              </MenuList>
+            </Menu>
+          )}
+          {!user && <Link to="/auth">Login</Link>}
+        </Flex>
       </Flex>
     </Flex>
   )
