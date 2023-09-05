5315bf5a5c3f8b6696d35ba9f30078fc962768ae
Carlos Junio <cjunior1@live.com>
2023-09-05 11:42:33 -0300

    feat: service add product to cart

---
+++ back-end/src/services/carts/addProductToCart.service.ts
@@ -0,0 +1,51 @@
+import AppDataSource from "../../data-source"
+import AppError from "../../error"
+import { Cart } from "../../entities/cart.entity"
+import { Product } from "../../entities/product.entity"
+import { ProductCart } from "../../entities/productCart.entity"
+import { User } from "../../entities/user.entity"
+import { ICart } from "../../interfaces/cart"
+
+const addProductToCartService = async (
+  { amount }: ICart,
+  productId: string,
+  userId: string
+): Promise<{ message: string }> => {
+  const cartRepo = AppDataSource.getRepository(Cart)
+  const productCartRepo = AppDataSource.getRepository(ProductCart)
+  const productRepo = AppDataSource.getRepository(Product)
+  const userRepo = AppDataSource.getRepository(User)
+
+  const product = await productRepo.findOneBy({
+    id: productId,
+  })
+
+  if (!product) {
+    throw new AppError("Product not found.", 404)
+  }
+
+  const user = await userRepo.findOneBy({ id: userId })
+
+  let cart = await cartRepo.findOne({
+    where: {
+      status: "PENDING",
+      user: { id: userId },
+    },
+  })
+
+  if (!cart) {
+    cart = cartRepo.create({ user: user! })
+
+    await cartRepo.save(cart)
+  }
+
+  await productCartRepo.save({
+    amount,
+    cart,
+    product,
+  })
+
+  return { message: "Product successfully added." }
+}
+
+export default addProductToCartService
