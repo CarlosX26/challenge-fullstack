e5804c7dfc5324ba3cbadc48e37c9d9b415207da
Carlos Junio <cjunior1@live.com>
2023-09-05 10:01:24 -0300

    feat: config user router

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,11 @@
 import "express-async-errors"
 import express from "express"
 import { handlerError } from "./error"
+import userRouter from "./routes/user.routes"
 
 const app = express()
+
+app.use("/users", userRouter)
 
 app.use(handlerError)
 
