5cdd8d660b4fa851b690e2c71701f7e5b6959f6c
Carlos Junio <cjunior1@live.com>
2023-09-05 15:53:59 -0300

    feat: add logo component

---
+++ front-end/src/components/Logo/index.tsx
@@ -0,0 +1,14 @@
+import { Flex, Heading } from "@chakra-ui/react"
+
+export const Logo = () => {
+  return (
+    <Flex gap="8px">
+      <Heading fontSize="lg" color="teal.600">
+        BEST
+      </Heading>
+      <Heading fontSize="lg" color="gray.700">
+        SHOP
+      </Heading>
+    </Flex>
+  )
+}
