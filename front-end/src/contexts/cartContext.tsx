82fc3ebf4e456a25edc9a0c4553bf556b4d8b6e0
Carlos Junio <cjunior1@live.com>
2023-09-06 13:22:19 -0300

    feat: implement methods of cart

--- front-end/src/contexts/cartContext.tsx
+++ front-end/src/contexts/cartContext.tsx
@@ -1,18 +1,50 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import { ICartProduct, ICartContext } from "./types"
 import { useDisclosure } from "@chakra-ui/hooks"
 import { toast } from "react-hot-toast"
+import { useAuthContext } from "./authContext"
+import { useNavigate } from "react-router-dom"
 import api from "../utils/axios"
 
 const CartContext = createContext({} as ICartContext)
 
 export const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const { isOpen, onClose, onOpen } = useDisclosure()
+  const navigate = useNavigate()
+  const { user } = useAuthContext()
 
   const [cartList, serCartList] = useState<ICartProduct[]>([])
 
+  useEffect(() => {
+    getCartList()
+  }, [])
+
+  const getCartList = async () => {
+    try {
+      const token = localStorage.getItem("@BESTSHOP:TOKEN")
+
+      const { data } = await api.get("/carts/view", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (!data) {
+        return null
+      }
+
+      serCartList(data.productCart)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const addProduct = async (productId: string) => {
     try {
+      if (!user) {
+        return navigate("/auth")
+      }
+
       const token = localStorage.getItem("@BESTSHOP:TOKEN")
 
       const { data } = await api.post(
@@ -31,11 +63,69 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
       toast.error("Produto já foi adicionado")
     }
   }
-  console.log(cartList)
+
+  const updateProduct = async (productId: string, amount: number) => {
+    try {
+      const token = localStorage.getItem("@BESTSHOP:TOKEN")
+
+      const { data } = await api.patch(
+        `/carts/products/${productId}`,
+        { amount },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+
+      const updateCartList = cartList.map((cartProduct) => {
+        if (cartProduct.product.id === productId) {
+          return data
+        }
+        return cartProduct
+      })
+
+      serCartList(updateCartList)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleteProduct = async (productId: string) => {
+    try {
+      const token = localStorage.getItem("@BESTSHOP:TOKEN")
+
+      await api.delete(
+        `/carts/products/${productId}`,
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+
+      const updateCartList = cartList.filter(
+        (cartProduct) => cartProduct.product.id !== productId
+      )
+
+      serCartList(updateCartList)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <CartContext.Provider
-      value={{ cartList, isOpen, onClose, onOpen, addProduct }}
+      value={{
+        cartList,
+        isOpen,
+        onClose,
+        onOpen,
+        addProduct,
+        updateProduct,
+        deleteProduct,
+      }}
     >
       {children}
     </CartContext.Provider>
