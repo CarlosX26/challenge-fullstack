4b89cdd1f643392cd8803fbca24d4f202cea0f65
Carlos Junio <cjunior1@live.com>
2023-09-06 20:19:17 -0300

    fix: feedback erros on forms auth

--- front-end/src/validations/auth.ts
+++ front-end/src/validations/auth.ts
@@ -1,14 +1,29 @@
 import { z } from "zod"
 
 const Login = z.object({
-  email: z.string().email("Email inválido.").nonempty("Campo vazio"),
+  email: z.string().nonempty("Campo vazio").email("Email inválido"),
   password: z.string().nonempty("Campo vazio"),
 })
 
 const Register = z.object({
-  name: z.string().nonempty("Campo vazio").min(3).max(128),
-  email: z.string().email("Email inválido.").nonempty("Campo vazio").max(128),
-  password: z.string().nonempty("Campo vazio"),
+  name: z
+    .string()
+    .nonempty("Campo vazio")
+    .min(3, "Mínimo 3 caracteres")
+    .max(128, "Máximo de caracteres 128"),
+  email: z
+    .string()
+    .nonempty("Campo vazio")
+    .email("Email inválido.")
+    .max(128, "Máximo de caracteres 128"),
+  password: z
+    .string()
+    .nonempty("Campo vazio")
+    .regex(/[A-Z]/, "Mínimo de 1 letra maiúscula")
+    .regex(/[a-z]/, "Mínimo de 1 letra minuscula")
+    .regex(/(\d)/, "Mínimo 1 número")
+    .regex(/(\W)|_/, "Mínimo de 1 caractere especial")
+    .regex(/(.{8,})|_/, "Mínimo de 8 caracteres"),
 })
 
 export { Login, Register }
