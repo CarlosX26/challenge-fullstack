be6fd3cfe4f865b1a22d6cbda87492cb432996f3
Carlos Junio <cjunior1@live.com>
2023-09-06 09:35:53 -0300

    feat: add method for update product

--- front-end/src/contexts/adminContext.tsx
+++ front-end/src/contexts/adminContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useContext } from "react"
-import { IAdminContext } from "./types"
+import { createContext, useContext, useEffect, useState } from "react"
+import { IAdminContext, IModal, IProductAdmin } from "./types"
 import { useDisclosure } from "@chakra-ui/react"
 import { IProduct } from "../validations/types"
 import { toast } from "react-hot-toast"
@@ -9,6 +9,48 @@ const AdminContext = createContext({} as IAdminContext)
 
 export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
   const { isOpen, onClose, onOpen } = useDisclosure()
+
+  const [adminProducts, setAdminProducts] = useState<IProductAdmin[]>([])
+  const [currentProduct, setCurrentProduct] = useState<IProductAdmin>()
+  const [modal, setModal] = useState<IModal>("addProduct")
+
+  const openModalAddProduct = () => {
+    setModal("addProduct")
+    onOpen()
+  }
+  const openModalEditProduct = () => {
+    setModal("editProduct")
+    onOpen()
+  }
+  const openModalDeleteProduct = () => {
+    setModal("deleteproduct")
+    onOpen()
+  }
+
+  useEffect(() => {
+    getAdminProducts()
+  }, [])
+
+  const handleProduct = (productId: string) => {
+    const product = adminProducts.find((product) => product.id === productId)
+    setCurrentProduct(product)
+  }
+
+  const getAdminProducts = async () => {
+    try {
+      const token = localStorage.getItem("@BESTSHOP:TOKEN")
+
+      const { data } = await api.get("/products/admin", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      setAdminProducts(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const createProduct = async (productData: IProduct) => {
     try {
@@ -28,8 +70,53 @@ export const AdminProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }
 
+  const updateProduct = async (productData: IProduct) => {
+    try {
+      const token = localStorage.getItem("@BESTSHOP:TOKEN")
+
+      const { data } = await api.patch(
+        `/products/${currentProduct?.id}`,
+        productData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+
+      const updateAdminProducts = adminProducts.map((product) => {
+        if (product.id === currentProduct?.id) {
+          return { ...currentProduct, ...data }
+        }
+        return product
+      })
+
+      setAdminProducts(updateAdminProducts)
+
+      toast.success("Produto atualizado")
+      onClose()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <AdminContext.Provider value={{ isOpen, onClose, onOpen, createProduct }}>
+    <AdminContext.Provider
+      value={{
+        isOpen,
+        modal,
+        currentProduct,
+        adminProducts,
+        onClose,
+        openModalAddProduct,
+        openModalEditProduct,
+        openModalDeleteProduct,
+        createProduct,
+        updateProduct,
+        handleProduct,
+      }}
+    >
       {children}
     </AdminContext.Provider>
   )
